1f985a689989e93d0da722a5310b41434505ffc6
Abrocoly <tomimoreno03@gmail.com>
2021-04-28 02:58:11 -0300

    añadiendo isChecked

--- components/Circle.jsx
+++ components/Circle.jsx
@@ -1,19 +1,23 @@
+import { useState } from 'react';
 import { useAppContext } from './Context';
 import usePosition from '../hooks/usePosition';
 
 export default function Circle({ condicionActual }) {
+  const [checked, setChecked] = useState(condicionActual.esVerdadero);
   const { respuesta, setArbol } = useAppContext();
   const pos = usePosition(condicionActual);
 
   const handleClick = () => {
     // eslint-disable-next-line no-param-reassign
     condicionActual.esVerdadero = !condicionActual.esVerdadero;
+    setChecked((curent) => !curent);
     setArbol(condicionActual.x);
   };
   return (
     <g onClick={handleClick}>
       <circle cursor="pointer" cx={pos.x} cy={pos.y} r="40" fill="blue" stroke="black" strokeWidth="4" />
-      {respuesta.includes(condicionActual) && <circle cursor="pointer" cx={pos.x} cy={pos.y} r="40" fill="blue" stroke="green" strokeWidth="4" clipPath="url(#left-to-right)" />}
+      {respuesta.includes(condicionActual) && <circle cursor="pointer" cx={pos.x} cy={pos.y} r="40" fill="blue" stroke={checked ? 'green' : 'red'} strokeWidth="4" clipPath="url(#left-to-right)" />}
+      <text cursor="pointer" textAnchor="middle" fill="Red" x={pos.x} y={pos.y + 13} fontSize="36">{checked ? '✅' : '⛔'}</text>
     </g>
   );
 }
--- components/Line.jsx
+++ components/Line.jsx
@@ -6,10 +6,11 @@ export default function Line({ punto1, punto2 }) {
   const pos1 = usePosition(punto1);
   const pos2 = usePosition(punto2);
   const X2 = punto2.tipo === 'accion' ? pos2.x - 50 : pos2.x;
+  const colorLinea = punto1.esVerdadero ? 'green' : 'red';
   return (
     <>
       <line x1={pos1.x} y1={pos1.y} x2={X2} y2={pos2.y} stroke="rgb(0,0,0)" strokeWidth="4" />
-      {respuesta.includes(punto1) && respuesta.includes(punto2) && <line x1={pos1.x} y1={pos1.y} x2={X2} y2={pos2.y} stroke="green" strokeWidth="4" clipPath="url(#left-to-right)" />}
+      {respuesta.includes(punto1) && respuesta.includes(punto2) && <line x1={pos1.x} y1={pos1.y} x2={X2} y2={pos2.y} stroke={colorLinea} strokeWidth="4" clipPath="url(#left-to-right)" />}
     </>
   );
 }
