72d6d531999bd9af03e220f1aa65b84882aff2a6
Abrocoly <tomimoreno03@gmail.com>
2021-05-05 03:29:09 -0300

    Conectando con base de datos
    Co-authored-by: Thiago-cpu <Thiago-cpu@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import ReactTooltip from 'react-tooltip';
+import { MongoClient } from 'mongodb';
 import Head from 'next/head';
 import styles from '../styles/Arbol.module.css';
 import Arbol from '../components/Arbol';
 import Tabla from '../components/Tabla';
 import { useAppContext } from '../components/Context';
 
-export default function Home() {
+export default function Home({ arbol }) {
   const { respuesta } = useAppContext();
+  console.log(arbol)
   return (
     <div className={styles.container}>
       <Head>
@@ -18,4 +20,17 @@ export default function Home() {
       <ReactTooltip type="dark" className={styles.tooltip} />
     </div>
   );
+}
+export async function getStaticProps() {
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.alkwu.mongodb.net/btree?retryWrites=true&w=majority`;
+  const client = await MongoClient.connect(uri);
+  const database = await client.db('btree');
+  const trees = await database.collection('trees');
+
+  const query = {};
+  const tree = await trees.findOne(query);
+  const parsedTree = JSON.parse(JSON.stringify(tree));
+  return {
+    props: { arbol: parsedTree },
+  };
 }
